e87ca8939bb6973268ef5d2c8ed4afd113648f0a
ÄÄVedrana Avramovic <avramovicvedrana@gmail.com>
2023-10-24 00:25:07 +0200

    Displays errors when user does not exist

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -4,9 +4,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import retry.RetryAnalyzer;
 
-public class LoginTests extends BasicTest{
+public class LoginTests extends BasicTest {
     @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
-    public void visitsTheLoginPage () {
+    public void visitsTheLoginPage() {
         navPage.clickOnLanguagesButton();
         navPage.clickOnEnLanguageButton();
         navPage.clickOnLoginButton();
@@ -14,8 +14,9 @@ public class LoginTests extends BasicTest{
         Assert.assertTrue(pageUrl.isLoginPage(),
                 "Should be redirected to the Login page.");
     }
-    @Test (priority = 2, retryAnalyzer = RetryAnalyzer.class)
-    public void checksInputTypes () {
+
+    @Test(priority = 2, retryAnalyzer = RetryAnalyzer.class)
+    public void checksInputTypes() {
         navPage.clickOnLoginButton();
 
         Assert.assertEquals(loginPage.getEmailInputType(), "email",
@@ -23,5 +24,21 @@ public class LoginTests extends BasicTest{
 
         Assert.assertEquals(loginPage.getPasswordInputType(), "password",
                 "The attribute type for email field should be 'password'.");
+    }
+
+    @Test(priority = 3, retryAnalyzer = RetryAnalyzer.class)
+    public void displaysErrorsWhenUserDoesNotExist() {
+        String email = "non-existing-user@gmal.com";
+        String password = "password123";
+
+        navPage.clickOnLoginButton();
+        loginPage.login(email, password);
+        messagePopUpPage.waitForErrorPopUpToBeVisible();
+
+        Assert.assertEquals(messagePopUpPage.getErrorMessageText(), "User does not exists",
+                "Error message should be 'User does not exists'.");
+
+        Assert.assertTrue(pageUrl.isLoginPage(),
+                "Should be redirected to the Login page.");
     }
 }
